a47ccbe459c5c8caf70d3bf5ae6b5f94f326cc0f
Xavier Herwyn <xavier.herwyn@gmail.com>
2020-06-21 17:43:31 +0200

    :sparkles: : Adding entity Employee

---
+++ src/main/java/com/example/demo/bo/Employee.java
@@ -0,0 +1,60 @@
+package com.example.demo.bo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String name;
+
+    @Column(length = 3, nullable = false)
+    private Integer age;
+
+    public Employee() {
+    }
+
+    public Employee(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
