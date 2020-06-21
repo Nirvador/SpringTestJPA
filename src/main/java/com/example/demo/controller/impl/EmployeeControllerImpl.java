99a4be5ea8e8fb88e3d0a4216f45daf8d40b0e67
Xavier Herwyn <xavier.herwyn@gmail.com>
2020-06-22 00:43:40 +0200

    :sparkles: : Adding employee controller methods

--- src/main/java/com/example/demo/controller/impl/EmployeeControllerImpl.java
+++ src/main/java/com/example/demo/controller/impl/EmployeeControllerImpl.java
@@ -2,12 +2,13 @@ package com.example.demo.controller.impl;
 
 import com.example.demo.bo.Employee;
 import com.example.demo.controller.EmployeeController;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.EmployeeRepository;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -28,5 +29,54 @@ public class EmployeeControllerImpl implements EmployeeController {
     @Override
     public List<Employee> getAllEmployee() {
         return ImmutableList.copyOf(employeeRepository.findAll());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ResponseEntity<Employee> getEmployee(String id) throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(Integer.valueOf(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for the id " + id));
+        return ResponseEntity.ok().body(employee);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ResponseEntity<Employee> updateEmployee(String id, Employee employeeSpecification)
+            throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(Integer.valueOf(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for the id " + id));
+
+        employee.setName(employeeSpecification.getName());
+        employee.setAge(employeeSpecification.getAge());
+
+        final Employee employeeUpdated = employeeRepository.save(employee);
+        return ResponseEntity.ok(employeeUpdated);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public HashMap<String, Boolean> deleteEmployee(String id) throws ResourceNotFoundException {
+        HashMap<String, Boolean> response = new HashMap<>();
+        Employee employee = employeeRepository.findById(Integer.valueOf(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for the id " + id));
+
+        employeeRepository.delete(employee);
+        response.put("deleted", true);
+        return response;
+
     }
 }
