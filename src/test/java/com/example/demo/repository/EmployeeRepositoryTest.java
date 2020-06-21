035e8249c6df59f8876a2457ed4dee4af8d3fce8
Xavier Herwyn <xavier.herwyn@gmail.com>
2020-06-21 18:36:04 +0200

    :white_check_mark: : Adding retrieval employee unit test

--- src/test/java/com/example/demo/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/demo/repository/EmployeeRepositoryTest.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class EmployeeRepositoryTest {
 
     @Autowired
@@ -20,7 +22,7 @@ public class EmployeeRepositoryTest {
     @Rollback(false)
     public void employeeRepository_shouldCreateAnEmployee() {
         // Given
-        Employee bruceBanner = new Employee("Bruce Banner", 35);
+        Employee bruceBanner = new Employee("Banner", 35);
 
         // When
         Employee employeeSaved = employeeRepository.save(bruceBanner);
@@ -29,4 +31,17 @@ public class EmployeeRepositoryTest {
         assertThat(employeeSaved.getId()).isGreaterThan(0);
         assertThat(employeeSaved).isEqualToComparingOnlyGivenFields(bruceBanner, "name", "age");
     }
+
+    @Test
+    public void employeeRepository_shouldFindAnEmployee() {
+        // Given
+        Employee tonyStark = employeeRepository.findByName("Stark");
+
+        // Then
+        assertThat(tonyStark.getName()).isEqualTo("Stark");
+    }
+
+
+
+
 }
